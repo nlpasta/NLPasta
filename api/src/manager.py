b207e144b35d2d730d80420030ed65a9be2d829e
Evan Hataishi <evan.hataishi@ibm.com@Evans-MacBook-Pro.local>
2019-07-11 11:43:28 -0700

    Fix api bugs

--- api/src/manager.py
+++ api/src/manager.py
@@ -4,3 +4,39 @@ import json
 def load_data(file_name):
     with open(file_name) as json_file:
         return json.load(json_file)
+
+
+def get_businesses(data):
+    businesses = []
+    for entry in data:
+        business = {}
+        for key, value in entry.items():
+            if key == 'reviews':
+                continue
+            business[key] = value
+
+        # if 'reviews' in business:
+        #     business.pop('reviews')
+            # del business['reviews']
+        businesses.append(business)
+    # businesses = {}
+    # for key in data.keys
+    return businesses
+
+
+def map_id_to_index(businesses):
+    map = {}
+    index = 0
+    for business in businesses:
+        map[business['business_id']] = index
+        index += 1
+    return map
+
+
+def get_reviews(id, business_index, data):
+    if id not in business_index:
+        return []
+    index = business_index[id]
+    reviews = data[index]['reviews']
+    return reviews
+
--- api/src/server.py
+++ api/src/server.py
@@ -7,14 +7,24 @@ app = Flask(__name__)
 CORS(app)
 
 data = []
+businesses = []
+business_index = {}
 
-@app.route('/api/reviews', methods=['GET'])
-def get_feedback():
-    return jsonify(data)
+
+@app.route('/api/businesses', methods=['GET'])
+def get_businesses():
+    return jsonify(businesses)
+
+
+@app.route('/api/business/<id>/reviews', methods=['GET'])
+def get_reviews(id):
+    return jsonify(manager.get_reviews(id, business_index, data))
 
 
 if __name__ == '__main__':
     data = manager.load_data('src/data.json')
+    businesses = manager.get_businesses(data)
+    business_index = manager.map_id_to_index(businesses)
     port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=True)
 
